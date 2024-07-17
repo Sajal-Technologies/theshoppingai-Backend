adff71b25207e7e7d4cec531b691ac7bce79be52
Adil Anwar <adil@sajaltech.com>
2024-07-17 12:32:14 +0530

    oxylabs product serach api and product detail api post to get

--- auths/views.py
+++ auths/views.py
@@ -1151,7 +1151,7 @@ logging.basicConfig(level=logging.DEBUG,
 
 
 class OxylabSearchView(APIView):
-    def post(self, request):
+    def get(self, request):
         logger = logging.getLogger(__name__)  # Get logger for this module
 
         # Log the incoming request details
@@ -1265,7 +1265,7 @@ import requests, json
 from pprint import pprint
 
 class OxylabProductDetailView(APIView):
-    def post(self,request):
+    def get(self,request):
         logger = logging.getLogger(__name__)  # Get logger for this module
 
         # Log the incoming request details
